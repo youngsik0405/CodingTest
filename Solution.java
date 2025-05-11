8a7769e3b80f126de5bab880f8e37bfaf8109bf4
Youngsik Yoon <dudtlr6512@naver.com>
2025-05-11 11:47:49 +0900

    홀수 짝수

---
+++ Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Solution {
+    public String solution(int num) {
+        return (num%2 == 0) ? "Odd" : "Even";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("정수를 입력하세요 :");
+        int num = sc.nextInt();
+    
+        Solution sol = new Solution();
+        String result = sol.solution(num);
+    
+        System.out.println("결과 :" + result);
+        
+        sc.close();
+    }
+}
+
